f0fe7996bf6f447d199dc066b11ffc5bfcbac0e2
Iori Mizutani <iori.mizutani@gmail.com>
2016-06-26 19:07:31 +0900

    rebuild concept

---
+++ README.md
@@ -0,0 +1,11 @@
+# gologi
+
+A simple LLRP-based logical reader mock for RFID Tags.
+
+## For what?
+
+You need to confirm the tag stream events in scenarios, but not really thrilled to use a heavily armed simulator like RIFIDI Edge suite.
+
+## Synopsis
+
+- TBA
--- main.go
+++ main.go
@@ -3,8 +3,8 @@ package main
 import (
 	"bytes"
 	"encoding/binary"
-	"encoding/hex"
 	"encoding/csv"
+	"encoding/hex"
 	"flag"
 	"fmt"
 	"io"
@@ -26,10 +26,10 @@ func check(e error) {
 
 func init() {
 	const (
-		defaultHost  = "127.0.0.1"
-		hostUsage    = "llrp client hostname"
-		defaultPort  = 5084
-		portUsage    = "port used for llrp connection"
+		defaultHost = "127.0.0.1"
+		hostUsage   = "llrp client hostname"
+		defaultPort = 5084
+		portUsage   = "port used for llrp connection"
 	)
 	flag.StringVar(&llrpHost, "host", defaultHost, hostUsage)
 	flag.StringVar(&llrpHost, "h",
@@ -39,11 +39,11 @@ func init() {
 		defaultPort, portUsage+" (shorthand)")
 }
 
-func buildPeakRSSIParameter() ([]byte) {
+func buildPeakRSSIParameter() []byte {
 	buf := new(bytes.Buffer)
 	var data = []interface{}{
-		uint8(134),		// 1+uint7(Type=6)
-		uint8(203),		// PeakRSSI
+		uint8(134), // 1+uint7(Type=6)
+		uint8(203), // PeakRSSI
 	}
 	for _, v := range data {
 		err := binary.Write(buf, binary.BigEndian, v)
@@ -52,12 +52,12 @@ func buildPeakRSSIParameter() ([]byte) {
 	return buf.Bytes()
 }
 
-func buildC1G2PCParameter(hexpc string) ([]byte) {
+func buildC1G2PCParameter(hexpc string) []byte {
 	buf := new(bytes.Buffer)
 	intpc, _ := strconv.ParseInt(hexpc, 10, 32)
 	var data = []interface{}{
-		uint8(140),		// 1+uint7(Type=12)
-		uint16(intpc),	// PC bits
+		uint8(140),    // 1+uint7(Type=12)
+		uint16(intpc), // PC bits
 	}
 	for _, v := range data {
 		err := binary.Write(buf, binary.BigEndian, v)
@@ -66,19 +66,19 @@ func buildC1G2PCParameter(hexpc string) ([]byte) {
 	return buf.Bytes()
 }
 
-func buildEPCDataParameter(length int64, epcLengthBits int64, epc []byte) ([]byte) {
+func buildEPCDataParameter(length int64, epcLengthBits int64, epc []byte) []byte {
 	var data []interface{}
 	if epcLengthBits == 96 {
 		data = []interface{}{
-			uint8(141),	// 1+uint7(Type=13)
-			epc,		// 96-bit EPCData string
+			uint8(141), // 1+uint7(Type=13)
+			epc,        // 96-bit EPCData string
 		}
 	} else {
 		data = []interface{}{
-			uint16(241),			// uint8(0)+uint8(Type=241)
-			uint16(length),			// Length
-			uint16(epcLengthBits),	// EPCLengthBits
-			epc,	// EPCData string
+			uint16(241),           // uint8(0)+uint8(Type=241)
+			uint16(length),        // Length
+			uint16(epcLengthBits), // EPCLengthBits
+			epc, // EPCData string
 		}
 	}
 	buf := new(bytes.Buffer)
@@ -89,15 +89,15 @@ func buildEPCDataParameter(length int64, epcLengthBits int64, epc []byte) ([]byt
 	return buf.Bytes()
 }
 
-func buildC1G2ReadOpSpecResultParameter(readData []byte) ([]byte) {
+func buildC1G2ReadOpSpecResultParameter(readData []byte) []byte {
 	buf := new(bytes.Buffer)
 	var data = []interface{}{
-		uint16(349),	// Rsvd+Type=349
-		uint16(11),		// Length
-		uint8(0),		// Result
-		uint16(9),		// OpSpecID
-		uint16(1),		// ReadDataWordCount
-		readData,		// ReadData
+		uint16(349), // Rsvd+Type=349
+		uint16(11),  // Length
+		uint8(0),    // Result
+		uint16(9),   // OpSpecID
+		uint16(1),   // ReadDataWordCount
+		readData,    // ReadData
 	}
 	for _, v := range data {
 		err := binary.Write(buf, binary.BigEndian, v)
@@ -109,14 +109,14 @@ func buildC1G2ReadOpSpecResultParameter(readData []byte) ([]byte) {
 func buildTagReportDataParameter(epcDataParameter []byte,
 	peakRSSIParameter []byte,
 	airProtocolTagDataParameter []byte,
-	opSpecResultParameter []byte) ([]byte) {
+	opSpecResultParameter []byte) []byte {
 	tagReportDataLength := len(epcDataParameter) +
 		len(peakRSSIParameter) + len(airProtocolTagDataParameter) +
 		len(opSpecResultParameter) + 4 // Rsvd+Type+length=32bits
 	buf := new(bytes.Buffer)
 	var data = []interface{}{
-		uint16(240),	// Reserved+Type=240 (TagReportData parameter)
-		uint16(tagReportDataLength),	// Length
+		uint16(240),                 // Reserved+Type=240 (TagReportData parameter)
+		uint16(tagReportDataLength), // Length
 		epcDataParameter,
 		peakRSSIParameter,
 		airProtocolTagDataParameter,
@@ -129,15 +129,15 @@ func buildTagReportDataParameter(epcDataParameter []byte,
 	return buf.Bytes()
 }
 
-func bufferROAccessReport(tagReportDataParameter []byte) (*bytes.Buffer) {
+func bufferROAccessReport(tagReportDataParameter []byte) *bytes.Buffer {
 	roAccessReportLength :=
 		len(tagReportDataParameter) + 10 // See below
 	messageID += 1
 	buf := new(bytes.Buffer)
 	var data = []interface{}{
-		uint16(1085),		// Rsvd+Ver+Type=61 (RO_ACCESS_REPORT)
-		uint32(roAccessReportLength),	// Message length
-		uint32(messageID),					// Message ID
+		uint16(1085),                 // Rsvd+Ver+Type=61 (RO_ACCESS_REPORT)
+		uint32(roAccessReportLength), // Message length
+		uint32(messageID),            // Message ID
 		tagReportDataParameter,
 	}
 	for _, v := range data {
@@ -148,9 +148,9 @@ func bufferROAccessReport(tagReportDataParameter []byte) (*bytes.Buffer) {
 }
 
 func Use(vals ...interface{}) {
-    for _, val := range vals {
-        _ = val
-    }
+	for _, val := range vals {
+		_ = val
+	}
 }
 
 func main() {
@@ -217,6 +217,11 @@ func main() {
 
 		// Send
 		fmt.Fprint(conn, roAccessReportBuffer)
+		fmt.Printf("%v\n", roAccessReportBuffer.Len())
 		fmt.Printf("% x\n", roAccessReportBuffer.Bytes())
+
+		// Wait until ACK received
+		buf := make([]byte, BUFSIZE)
+		_, err := conn.Read(buf)
 	}
 }
