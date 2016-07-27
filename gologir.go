efa29cc6e27949cfe70fdcccf94f0d4d2d4262b6
Iori Mizutani <iori.mizutani@gmail.com>
2016-07-28 01:41:21 +0900

    add test and .travis.yml

--- gologir.go
+++ gologir.go
@@ -1,4 +1,4 @@
-package main
+package gologir
 
 import (
 	"encoding/binary"
@@ -50,7 +50,7 @@ var (
 	keepaliveID = *initialKeepaliveID
 )
 
-func Check(e error) {
+func check(e error) {
 	if e != nil {
 		panic(e)
 	}
---
+++ gologir_test.go
@@ -0,0 +1,7 @@
+package gologir
+
+import (
+	"testing"
+)
+
+
