268b24df708a6a907cc1a2d8eb11a6c007502ff8
Iori Mizutani <iori.mizutani@gmail.com>
2017-11-17 20:09:53 +0900

    move util cmds to go-llrp

--- cmd/uiigen/iso.go
+++
@@ -1,8 +0,0 @@
-// This file contains ISO binary encoding scheme
-package main
-
-// MakeRuneSliceOfISO17367 generates a random 17367 code
-func MakeRuneSliceOfISO17367() []byte {
-
-	return []byte{}
-}
